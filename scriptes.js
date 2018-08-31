379d1632720c4be4813407d849b600a6c7ebd02a
Mabeesha Wijekoon <31009864+Mabeesha@users.noreply.github.com>
2018-08-31 20:16:08 +0530

    Add files via upload

---
+++ scriptes.js
@@ -0,0 +1,7 @@
+function onLoadChangeOpacity()
+{
+	document.getElementById("divTop").style.opacity = 1;
+	document.getElementById("divTop").style.maxWidth = '700px';
+	document.getElementById("divTop").style.marginTop = '180px';
+	document.getElementById("divIntro").style.opacity = 1;
+}
