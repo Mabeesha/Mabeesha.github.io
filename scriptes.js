4294d8bd53491339b301fdc929922eec8fbff54b
Mabeesha Wijekoon <31009864+Mabeesha@users.noreply.github.com>
2018-09-28 00:49:08 +0530

    Add files via upload

--- scriptes.js
+++ scriptes.js
@@ -2,9 +2,28 @@ function onLoadChangeOpacity()
 {
 	document.getElementById("divTop").style.opacity = 1;
 	document.getElementById("divTop").style.maxWidth = '600px';
-	document.getElementById("divTop").style.marginTop = '75px';
+	document.getElementById("divTop").style.marginTop = '105px';
 	document.getElementById("divIntro").style.opacity = 1;
+	document.getElementById("background").style.opacity = 1;
 	//document.getElementById("divEducation").style.opacity = 1;
 	//document.getElementById("divSkills").style.opacity = 1;
 	//document.getElementById("divProjects").style.opacity = 1;
 }
+/*
+$('#divSkills').waypoint(function() 
+{
+    alert('You have scrolled to an entry.');
+}, 
+{
+    offset: '100%'
+}
+);*/
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
