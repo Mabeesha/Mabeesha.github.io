0df5cd321b5e1fce3f41955ec59ef960a3a11485
Mabeesha Wijekoon <31009864+Mabeesha@users.noreply.github.com>
2018-10-15 22:37:23 +0530

    style changes

--- scriptes.js
+++ scriptes.js
@@ -2,7 +2,7 @@ function onLoadChangeOpacity()
 {
 	document.getElementById("divTop").style.opacity = 1;
 	document.getElementById("divTop").style.maxWidth = '600px';
-	document.getElementById("divTop").style.marginTop = '105px';
+	document.getElementById("divTop").style.marginTop = '70px';
 	document.getElementById("divIntro").style.opacity = 1;
 	document.getElementById("background").style.opacity = 1;
 	//document.getElementById("divEducation").style.opacity = 1;
@@ -18,6 +18,7 @@ $('#divSkills').waypoint(function()
     offset: '100%'
 }
 );*/
+$(document).ready(function(){ 
 $("a[href^='#']").click(function(e) {
 	e.preventDefault();
 	
@@ -26,4 +27,21 @@ $("a[href^='#']").click(function(e) {
 	$("body, html").animate({
 		scrollTop: position
 	} /* speed */ );
+});
+});
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar").css('background-color', 'rgba(74,74,74,1)');
+       } else {
+          $('.navbar').css('background-color', 'rgba(74,74,74,0)');
+       }
+   });
+    }
 });
